49b05894e0cc109ee332846f0f2bf4df9bdc0ca4
Hyunji Kim <papertoy01@gmail.com>
2023-05-20 19:41:46 +0900

    조건문

---
+++ python/5_if.py
@@ -0,0 +1,12 @@
+print(1)
+if True: #들여쓰기 중요합니다!
+    print(2)
+if False:
+    print(3)
+print(4)
+
+비밀번호 = input("비밀번호 : ")
+if(비밀번호 == "1111"):
+    print("안녕하세요")
+if(비밀번호 != "1111"):
+    print("누구세요?")
